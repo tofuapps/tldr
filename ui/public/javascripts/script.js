34f05103836938fa58ea570b4312fb1d88d9cfad
lyc <22719973+l-yc@users.noreply.github.com>
2020-03-14 13:12:20 +0000

    added single article summary

--- ui/public/javascripts/script.js
+++ ui/public/javascripts/script.js
@@ -1,11 +1,77 @@
 feather.replace();
 
+const Modal = {
+  data: function () {
+    return {
+      title: '',
+      bodyContent: ''
+    }
+  },
+  methods: {
+    dismiss: function () {
+      document.body.removeChild(this.$el);
+    }
+  },
+  template: `
+        <div class="modal">
+            <div class="content">
+                <div class="title">
+                  {{ title }}
+                </div>
+                <form>
+                    <div class="row">
+                      <div v-if="bodyContent.length == 0" class="loader-box">
+                        <div class="loader"></div>
+                        <div class="message">Loading...</div>
+                      </div>
+                      <div v-else class="row">
+                        {{ bodyContent }}
+                      </div>
+                    </div>
+                    <div class="row submit">
+                        <input type="button" value="Ok" v-on:click="dismiss()">
+                        <input type="button" value="Cancel" v-on:click="dismiss()">
+                    </div>
+                </form>
+            </div>
+        </div>
+    `
+};
+
 var app = new Vue({
   el: '#app',
   data: {
     activetab: 0,
     message: '', //'Hello Vue!',
-    newsfeed: []
+    newsfeed: [],
+  },
+  methods: {
+    displaySummary: function (article) {
+      var url = '/api/newsfeed/get_article_summary?'
+      var params = {
+        title: article.title,
+        link: article.url
+      }
+      url += new URLSearchParams(params).toString();
+      console.log(url);
+
+      var opts = {
+        method: 'GET',
+        headers: {}
+      };
+
+      let modal = makeModal2(null);
+      document.body.appendChild(modal.$el)
+      modal.title = article.title;
+
+      fetch(url, opts)
+        .then(response => response.json())
+        .then(data => {
+          console.log(JSON.stringify(data));
+          console.log('displaying summary of article ' + data.summary);
+          modal.bodyContent = data.summary;
+        });
+    }
   }
 })
 
@@ -19,14 +85,30 @@ function docReady(fn) {
   }
 }
 
-function makeModal(article) {
+function makeModal2() {
+  var ComponentClass = Vue.extend(Modal)
+  var instance = new ComponentClass()
+  instance.$mount();
+  console.log(instance);
+  return instance;
+}
+
+function makeModal() {
   let sampleModal = `
         <div class="modal">
             <div class="content">
                 <div class="title">
+                  {{ modalContent.title }}
                 </div>
                 <form>
                     <div class="row">
+                      <div v-if="modalContent.bodyContent.length == 0" class="loader-box">
+                        <div class="loader"></div>
+                        <div class="message">Loading...</div>
+                      </div>
+                      <div v-else class="row">
+                        {{ modalContent.bodyContent }}
+                      </div>
                     </div>
                     <div class="row submit">
                         <input type="button" value="Ok">
@@ -68,28 +150,4 @@ docReady(() => {
       //  newsfeed.articles.push(data[0].articles[i]);
       //}
     });
-
-  document.querySelector('#headlines-container').addEventListener('click', e => {
-    console.log(e.target);
-    if (e.target.classList.contains('single-summary-btn')) {
-      document.body.appendChild(makeModal(null));
-    }
-  });
-  document.body.appendChild(makeModal(null));
-
-  //document.getElementById('summarise-btn').onclick = () => {
-  //  let modal = document.createElement('div');
-  //  modal.innerHTML = sampleModal;
-
-  //  modal.querySelector('input[value="Ok"]').addEventListener('click', () => {
-  //    document.body.removeChild(modal);
-  //  });
-
-  //  modal.querySelector('input[value="Cancel"]').addEventListener('click', () => {
-  //    document.body.removeChild(modal);
-  //  });
-
-  //  document.body.appendChild(modal);
-  //};
-
 });
