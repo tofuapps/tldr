ffa820c31c5c7abeb19f5d639625137f162ad3a0
Wern Jie Lim <lwjdev@gmail.com>
2020-03-27 21:37:53 +0800

    added caching for simple_fetch and retrieve_article_info

--- fetcher/fetcher.py
+++ fetcher/fetcher.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+import os
 import json
 import requests
 import feedparser
-import utils.utils as utils
 import newspaper
+from datetime import datetime
+import utils.utils as utils
 
 class Fetcher:
+    """ Supplies the news articles """
+
     feeds = [
         'https://www.channelnewsasia.com/rssfeeds/8395986',
         'http://feeds.bbci.co.uk/news/rss.xml',
@@ -13,9 +17,12 @@ class Fetcher:
         #'https://news.google.com/rss/'
     ]
 
-    """ Supplies the news articles """
-    def __init__(self):
-        pass
+    def __init__(self, use_storage_for_cache=True):
+        self.use_storage_for_cache = use_storage_for_cache
+        self.__cached_simple_fetch = {}
+        self.__cached_articles_info = {}
+        if use_storage_for_cache:
+            self.readCacheFromStorage()
 
     def fetch(self, urls=None, debug=False):
         """"Fetches list of news from a list of rss feeds and returns the raw result."""
@@ -41,10 +48,11 @@ class Fetcher:
         return res
 
 
-
-    def simple_fetch(self, url=None):
+    def simple_fetch(self, cached=False, url=None):
         """
         Fetches list of news from an rss feed, and returns it in a simplified form with basic types.
+
+        The results of the rss feed to avoid multiple requests by using the parameter cached=True. The default is False.
 
         An array of news in the form of dictionaries with keys:
            - url (string)
@@ -53,22 +61,32 @@ class Fetcher:
            - short_summary (plain text string)
         are returned.
         """
-        data = self.fetch(url)
+        CACHE_KEY = ("URL<%s>" % str(url))
 
-        final_data = list(map(lambda entry: \
-                              {
-                                  "url": entry.links[0].href,
-                                  "title": entry.title,
-                                  "published_on": entry.published_parsed,
-                                  "short_summary": (None if not "summary" in entry else utils.clean_html(entry.summary))
-                              },
-                              data))
+        if cached and CACHE_KEY in self.__cached_simple_fetch:
+            final_data = self.__cached_simple_fetch[CACHE_KEY]
+        else:
+            data = self.fetch(url)
+            final_data = list(map(lambda entry: \
+                                  {
+                                      "url": entry.links[0].href,
+                                      "title": entry.title,
+                                      "published_on": entry.published_parsed,
+                                      "short_summary": (None if not "summary" in entry else utils.clean_html(entry.summary))
+                                  },
+                                  data))
+            if cached and final_data:
+                self.__cached_simple_fetch[CACHE_KEY] = final_data
+                if self.use_storage_for_cache:
+                    self.saveCacheToStorage()
 
         return final_data
 
-    def retrieve_article_info(self, url):
+    def retrieve_article_info(self, url, cached=True):
         """
-        Retrieves the text contents of a url pointing to an article in a dictionary. Does not include the title of the article.
+        Retrieves the article raw info from a url in a dictionary.
+
+        This method also returns the cached contents of the url if possible, and can be overriden via the parameter cached=False.
 
         The dictionary consists of the following keys:
             - text (string)
@@ -76,26 +94,50 @@ class Fetcher:
             - status_code (int)
             - success (boolean)
         """
-        response = requests.get(url)
+        CACHE_KEY = ("URL<%s>" % str(url))
 
-        result = {}
-        result["text"] = response.text
-        result["status_code"] = response.status_code
-        if 200 <= response.status_code < 300:
-            result["success"] = True
-            result["plain_text"] = newspaper.fulltext(response.text)
+        if CACHE_KEY in self.__cached_articles_info:
+            result = self.__cached_articles_info[CACHE_KEY]
         else:
-            result["success"] = False
+            response = requests.get(url)
+
+            result = {}
+            result["text"] = response.text
+            result["status_code"] = response.status_code
+            if 200 <= response.status_code < 300:
+                result["success"] = True
+                result["plain_text"] = newspaper.fulltext(response.text)
+                if cached:
+                    self.__cached_articles_info[CACHE_KEY] = result
+                    if self.use_storage_for_cache:
+                        self.saveCacheToStorage()
+            else:
+                result["success"] = False
 
         return result
 
     def retrieve_article_contents(self, url):
-        """Retrieves the text contents of a url pointing to an article. Does not include the title of the article."""
-        print("WARNING: retrieve_article_contents used is deprecated.")
+        """Retrieves the text contents of a url pointing to an article. """
+        print("WARNING: retrieve_article_contents method is deprecated.")
         data = self.retrieve_article_info(url)
         if not data["success"]:
             return "Error " + str(data["status_code"]) + "."
         return data["plain_text"]
+
+    def saveCacheToStorage(self):
+        with open('fetcher__cached_simple_fetch.json', 'w') as out:
+            out.write(json.dumps(self.__cached_simple_fetch))
+        with open('fetcher__cached_articles_info.json', 'w') as out:
+            out.write(json.dumps(self.__cached_articles_info))
+
+    def readCacheFromStorage(self):
+        if os.path.isfile('fetcher__cached_simple_fetch.json'):
+            with open('fetcher__cached_simple_fetch.json', 'r') as file:
+                self.__cached_simple_fetch = json.loads(file.read())
+        if os.path.isfile('fetcher__cached_articles_info.json'):
+            with open('fetcher__cached_articles_info.json', 'r') as file:
+                self.__cached_articles_info = json.loads(file.read())
+
 
 if __name__ == '__main__':
     fetcher = Fetcher()
