78d855550a6cd4687d8553f0903b2391466e3257
lyc <22719973+l-yc@users.noreply.github.com>
2020-04-03 07:26:08 +0000

    cleaned up backend servers

--- api_server.py
+++ api_server.py
@@ -8,9 +8,7 @@ from summarizer.summarizer import Summarizer
 
 import json
 import sys
-import argparse
 
-import pprint
 import traceback
 import time
 
@@ -21,100 +19,77 @@ summarizer = Summarizer()
 
 app = Flask(__name__)
 
+# flask routes
 @app.route('/')
 def index():
     return "Hello, Home!"
 
 @app.errorhandler(404)
 def not_found(error):
-    print(request)
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    print('[ERROR] Offending request: ', request)
+    return make_response(jsonify({'success': False, 'error': 'Not found'}), 404)
 
 @app.errorhandler(Exception)
 def handle_error(e):
     tb = traceback.format_exc()
+    print('[ERROR] Exception: ', str(e))
     print(tb)
     code = 500
     if isinstance(e, HTTPException):
         code = e.code
     return jsonify(error=str(e)), code
 
+# api routes
 @app.route('/api/v1.0/newsfeed/get', methods=['GET'])
 def get_newsfeed():
-    #cluster = request.args.get('title')
-
     articles = fetcher.simple_fetch()
     result = curator.curate(articles, use_cluster=False)
-
-    print(result)
     return jsonify(result)
 
 @app.route('/api/v1.0/newsfeed/get_article_summary', methods=['POST'])
 def get_article_summary():
-    #title = request.args.get('title')
-    #link = request.args.get('link')
-    print('got a request')
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(request.data)
-    #pp.pprint(request.values)
-    print('hello ' , request.json['articles'][0])
     articles = request.get_json()['articles']
-    print(articles)
 
-    #print('got params ', title, link)
-    print('got request')
-    #print("AAAAAAAAAAAAAAAAAAAAAAA ", type(articles), articles)
-    t00 = time.time()
-    t0 = time.time()
-    print("[{}] starting ".format(time.time()-t0), file=sys.stderr); t0 = time.time();
     filled = []
     if articles is not None:
         for article in articles:
             if article['url'] is not None:
-            #title is not None and link is not None:
                 res = fetcher.retrieve_article_info(article['url'])
-                print(res['success'])
                 article_contents = res['plain_text'] if res['success'] else None
                 filled.append({
                     'title': article['title'],
                     'passage': article_contents
                 })
     result = summarizer.summarize(filled, query=None)
-    print("[{}] TOTAL ELAPSED END".format(time.time()-t00), file=sys.stderr)
-    print("RESULT ", result)
-    result['error'] = False
+    #print("[TIMER][{}] TOTAL ELAPSED END".format(time.time()-t00), file=sys.stderr)
     return jsonify(result)
 
 @app.route('/api/v1.0/newsfeed/get_query_summary', methods=['GET'])
 def get_query_summary():
     query = request.args.get('query')
 
-    t00 = time.time()
-    t0 = time.time()
-    print("[{}] starting ".format(time.time()-t0), file=sys.stderr); t0 = time.time();
-    summarizer = Summarizer()
+    #t00 = time.time()
+    #t0 = time.time()
+    #print("[TIMER][{}] Starting...".format(time.time()-t0), file=sys.stderr); t0 = time.time();
     filled = []
-    print("[{}] before simple_fetch ".format(time.time()-t0), file=sys.stderr)
     articles = fetcher.simple_fetch()
-    print("[{}] did simple_fetch ".format(time.time()-t0), file=sys.stderr)
-    #for a in articles:
-    for idx, a in enumerate(articles):  # TODO: can we check through all the articles quickly?
+    #print("[TIMER][{}] Finished simple_fetch.".format(time.time()-t0), file=sys.stderr)
+    for a in articles: # TODO: can we check through all the articles quickly?
         res = fetcher.retrieve_article_info(a['url'])
         article_contents = res['plain_text'] if res['success'] else None
         filled.append({
             'title': a['title'],
             'passage': article_contents
         })
-        print("[{}] article {} done".format(time.time()-t0,idx), file=sys.stderr); t0 = time.time()
+        #print("[TIMER][{}] Fetched article {}.".format(time.time()-t0,idx), file=sys.stderr); t0 = time.time()
     result = summarizer.summarize(filled, query=query)
-    print("[{}] TOTAL ELAPSED END".format(time.time()-t00), file=sys.stderr)
+    #print("[TIMER][{}] TOTAL ELAPSED END".format(time.time()-t00), file=sys.stderr)
     return jsonify(result)
 
 if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0',port=8888,debug=True)
     except Exception as e:
-        print('exception: ', e)
         tb = traceback.format_exc()
         print(tb)
         raise e
--- ui/routes/api.js
+++ ui/routes/api.js
@@ -1,83 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-var path = require('path');
-const spawn = require("child_process").spawn;
-
-//var proxy = require('http-proxy-middleware');
-//router = proxy({target: 'https://yourdomain.com', changeOrigin: true});
-
-module.exports = router;
-
-//router.get('/get', function(req, res, next) {
-//  console.log(global.config.apiEndpoint);
-//    res.json({ success: true, apiEndpoint: global.config.apiEndpoint })
-//});
-//
-//router.get('/newsfeed/get', function(req, res, next) {
-//  //res.render('index', { title: 'Express' });
-//  //res.render('app');
-//  const pythonProcess = spawn('pipenv', ['run', 'python3', path.join(global.appRoot, '../main.py'), '--curate']);
-//  pythonProcess.on('error', (err) => {
-//    console.log('big oof ' + err);})
-//
-//  pythonProcess.stdout.on('data', (data) => {
-//    // Do something with the data returned from python script
-//    //console.log('got data??? ' + data.toString());
-//    //res.status(400).json({ success: true, message: 'Success.', newsfeed: data.toString() });
-//    res.write(data);
-//  });
-//  pythonProcess.stdout.on('end', () => {
-//    console.log('done')
-//    res.end();
-//  });
-//
-//  pythonProcess.stderr.on('data', (data) => {
-//    console.error(data.toString());
-//  });
-//});
-//
-//router.get('/newsfeed/get_article_summary', function(req, res, next) {
-//  //res.render('index', { title: 'Express' });
-//  //res.render('app');
-//  const pythonProcess = spawn('pipenv', ['run', 'python3', path.join(global.appRoot, '../main.py'),
-//    '-t', req.query.title, '-l', req.query.link, '--summarize']);
-//  pythonProcess.stdout.on('data', (data) => {
-//    res.write(data);
-//  });
-//  pythonProcess.stdout.on('end', () => {
-//    console.log('done')
-//    res.end();
-//  });
-//
-//  pythonProcess.stderr.on('data', (data) => {
-//    console.error(data.toString());
-//  });
-//});
-//
-//router.get('/newsfeed/get_query_summary', function(req, res, next) {
-//  //res.render('index', { title: 'Express' });
-//  //res.render('app');
-//  console.log('query is: ' + req.query.query);
-//  const pythonProcess = spawn('pipenv', ['run', 'python3', path.join(global.appRoot, '../main.py'),
-//    '-q', req.query.query, '--summarize']);
-//  pythonProcess.on('error', (err) => {
-//    console.log('big oof ' + err);
-//  });
-//  
-//  pythonProcess.stdout.on('data', (data) => {
-//    console.log('got data: ' + data.toString());
-//    res.write(data);
-//  });
-//  pythonProcess.stdout.on('end', () => {
-//    console.log('done')
-//    res.end();
-//  });
-//
-//  pythonProcess.stderr.on('data', (data) => {
-//    console.error(data.toString());
-//  });
-//});
-//
-//
-//module.exports = router;
+// api server is now separate, so we just create a proxy here
+module.exports = createProxyMiddleware({
+  target: global.config.apiEndpoint, // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+  pathRewrite: {
+    '^/api/v1.0': '' // remove base path
+  }
+});
